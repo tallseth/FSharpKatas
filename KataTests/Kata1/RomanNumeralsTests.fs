6c2cd88a7d71ab956bf2995009a08d39a3310755
Seth McCarthy <seth.mccarthy@olo.com>
2021-10-16 06:34:30 -0700

    Refactor to enable reuse of interesting tests
    This was a pain, there is probably an easier way.

--- KataTests/Kata1/RomanNumeralsTests.fs
+++ KataTests/Kata1/RomanNumeralsTests.fs
@@ -4,26 +4,37 @@ open Katas.Kata1
 open Xunit
 open FsUnit.Xunit
 
-[<Theory>]
-[<InlineData("I", 1)>]
-[<InlineData("II", 2)>]
-[<InlineData("III", 3)>]
-[<InlineData("IV", 4)>]
-[<InlineData("V", 5)>]
-[<InlineData("VIII", 8)>]
-[<InlineData("IX", 9)>]
-[<InlineData("X", 10)>]
-[<InlineData("XIV", 14)>]
-[<InlineData("XVII", 17)>]
-[<InlineData("XXXIX", 39)>]
-[<InlineData("XLIX", 49)>]
-[<InlineData("XLVII", 47)>]
-[<InlineData("XCIV", 94)>]
-[<InlineData("CCXLIV", 244)>]
-[<InlineData("MCMLXXX", 1980)>]
-[<InlineData("MMXIX", 2019)>]
-[<InlineData("CDII", 402)>]
-[<InlineData("DXXXVII", 537)>]
-let ``Valid roman numerals give correct results`` input expected =
-    let actual = RomanNumerals.fromRomanNumeral input
-    actual |> should equal expected
+type RomanNumeralPair = {StringForm:string; NumericForm:int}
+
+type RomanNumeralTests() = 
+    static member ValidNumeralPairs
+        with get() =
+            [|
+                {StringForm = "I"; NumericForm = 1}
+                {StringForm ="I"; NumericForm =  1}
+                {StringForm ="II"; NumericForm =  2}
+                {StringForm ="III"; NumericForm =  3}
+                {StringForm ="IV"; NumericForm =  4}
+                {StringForm ="V"; NumericForm =  5}
+                {StringForm ="VIII"; NumericForm =  8}
+                {StringForm ="IX"; NumericForm =  9}
+                {StringForm ="X"; NumericForm =  10}
+                {StringForm ="XIV"; NumericForm =  14}
+                {StringForm ="XVII"; NumericForm =  17}
+                {StringForm ="XXXIX"; NumericForm =  39}
+                {StringForm ="XLIX"; NumericForm =  49}
+                {StringForm ="XLVII"; NumericForm =  47}
+                {StringForm ="XCIV"; NumericForm =  94}
+                {StringForm ="CCXLIV"; NumericForm =  244}
+                {StringForm ="MCMLXXX"; NumericForm =  1980}
+                {StringForm ="MMXIX"; NumericForm =  2019}
+                {StringForm ="CDII"; NumericForm =  402}
+                {StringForm ="DXXXVII"; NumericForm =  537}                
+            |]
+            |> Array.map (fun x-> [|x|])
+
+    [<Theory>]
+    [<MemberData("ValidNumeralPairs")>]
+    member this.``Valid roman numerals give correct results`` (testCase:RomanNumeralPair) =
+        let actual = RomanNumerals.fromRomanNumeral testCase.StringForm
+        actual |> should equal testCase.NumericForm
