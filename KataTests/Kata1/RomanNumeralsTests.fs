c9415976c25e59af07b9a0c087f3b243bb1acf5a
Seth McCarthy <seth.mccarthy@olo.com>
2021-10-16 06:04:20 -0700

    First part of Kata1: Roman Numerals
    Just parsing strings to numbers now, and not handling any creative
    edge cases.

---
+++ KataTests/Kata1/RomanNumeralsTests.fs
@@ -0,0 +1,29 @@
+﻿module KataTests.Kata1.RomanNumeralsTests
+
+open Katas.Kata1
+open Xunit
+open FsUnit.Xunit
+
+[<Theory>]
+[<InlineData("I", 1)>]
+[<InlineData("II", 2)>]
+[<InlineData("III", 3)>]
+[<InlineData("IV", 4)>]
+[<InlineData("V", 5)>]
+[<InlineData("VIII", 8)>]
+[<InlineData("IX", 9)>]
+[<InlineData("X", 10)>]
+[<InlineData("XIV", 14)>]
+[<InlineData("XVII", 17)>]
+[<InlineData("XXXIX", 39)>]
+[<InlineData("XLIX", 49)>]
+[<InlineData("XLVII", 47)>]
+[<InlineData("XCIV", 94)>]
+[<InlineData("CCXLIV", 244)>]
+[<InlineData("MCMLXXX", 1980)>]
+[<InlineData("MMXIX", 2019)>]
+[<InlineData("CDII", 402)>]
+[<InlineData("DXXXVII", 537)>]
+let ``Valid roman numerals give correct results`` input expected =
+    let actual = RomanNumerals.fromRomanNumeral input
+    actual |> should equal expected
---
+++ Katas/Kata1/RomanNumerals.fs
@@ -0,0 +1,36 @@
+﻿module Katas.Kata1.RomanNumerals
+
+let I = "I", 1
+let IV = "IV", 4
+let V = "V", 5
+let IX = "IX", 9
+let X = "X", 10
+let XL = "XL", 40
+let L = "L", 50
+let XC = "XC", 90
+let C = "C", 100
+let CD = "CD", 400
+let D = "D", 500
+let CM = "CM", 900
+let M = "M", 1000
+
+let alphabet = [I; IV; V; IX; X; XL; L; XC; C; CD; D; CM; M]
+
+
+let getGlyphFromString (romanNumeral:string) =
+    let twoLetterMatch = List.tryFind (fun x -> (fst x) = romanNumeral.[0..1]) alphabet
+    let oneLetterMatch = List.tryFind (fun x -> (fst x) = romanNumeral.[0..0]) alphabet
+    match (twoLetterMatch, oneLetterMatch) with
+    | Some x, _ -> x
+    | None, Some x -> x
+    | _ -> failwith "Invalid Numeral"
+
+
+let rec private fromRomanNumeralRecursive baseNumber (romanNumeral:string) =
+    match romanNumeral.Length with
+    | 0 -> baseNumber
+    | _ ->
+        let currentSymbol, currentNumber = getGlyphFromString romanNumeral
+        fromRomanNumeralRecursive (baseNumber + currentNumber) (romanNumeral.Substring currentSymbol.Length)
+
+let fromRomanNumeral = fromRomanNumeralRecursive 0
