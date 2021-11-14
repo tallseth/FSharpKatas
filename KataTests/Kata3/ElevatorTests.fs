f4a5b4cad2de9e629ee2dc31e4d2ebf8a71d7462
Seth McCarthy <seth.mccarthy@olo.com>
2021-11-14 07:57:38 -0800

    Kata3: intial stub of elevator kata
    Took a stab at modeling with types, and laid out a trivial first
    test.

---
+++ KataTests/Kata3/ElevatorTests.fs
@@ -0,0 +1,17 @@
+﻿module KataTests.Kata3.ElevatorTests
+
+open Xunit
+open Katas.Kata3.Elevator
+open FsUnit.Xunit
+
+
+
+[<Fact>]
+let ``Simple "closest elevator" scenario``() =
+    let startingState = 1,2
+    let calls = [0, Up; 4, Down]
+    
+    let response = determineElevatorResponse startingState calls
+    
+    response |> should equal [Left; Right]
+    
---
+++ Katas/Kata3/Elevator.fs
@@ -0,0 +1,16 @@
+﻿module Katas.Kata3.Elevator
+
+type Direction = Up | Down
+
+type Floor = int
+
+type Elevator = Left | Right
+
+type ElevatorCall = Floor * Direction
+
+type ElevatorState = Floor * Floor
+
+
+
+let determineElevatorResponse (state:ElevatorState) (calls:ElevatorCall list) =
+    [Left; Right]
