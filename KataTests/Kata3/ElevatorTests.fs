6f432ca2eb30b4e8242cb32122767bf493f74e90
Seth McCarthy <seth.mccarthy@olo.com>
2021-11-14 10:48:09 -0800

    All but requirement 'e'

--- KataTests/Kata3/ElevatorTests.fs
+++ KataTests/Kata3/ElevatorTests.fs
@@ -5,24 +5,32 @@ open Katas.Kata3.Elevator
 open FsUnit.Xunit
 
 [<Fact>]
-let ``Simple "closest elevator" scenario``() =
-    let response = determineElevatorResponse 1 2 (0, Up) (4, Down)
+let ``From examples on the wiki page``() =
+    //The closest elevator to a call should take the call.
+    determineElevatorResponse 0 3 (3, Down) (0, Up) |> should equal (Right, Left)
+    determineElevatorResponse 0 3 (1, Down) (4, Down) |> should equal (Left, Right)
     
-    response |> should equal (Left, Right)
+    //If both elevators are equally close to the call, the default elevator is 'right'
+    determineElevatorResponse 2 2 (2, Down) (3, Up) |> should equal (Right, Left)
+    determineElevatorResponse 1 1 (2, Down) (2, Up) |> should equal (Right, Left)
     
-[<Fact>]
-let ``Reverse of Simple "closest elevator" scenario``() =
-    let response = determineElevatorResponse 2 1 (0, Up) (4, Down)
-    
-    response |> should equal (Right, Left)
+    //An elevator will take both calls only if ALL of these conditions are met:
 
-[<Fact>]
-let ``All distances equal prefer the right elevator for the first call``() =
-    let response = determineElevatorResponse 1 1 (0, Up) (0, Down)
+    //Both calls are in the same direction
+    determineElevatorResponse 0 0 (1, Up) (3, Up) |> should equal (Right, Right)
+    determineElevatorResponse 4 4 (3, Down) (2, Down) |> should equal (Right, Right)
+    determineElevatorResponse 4 4 (3, Down) (2, Up) |> should equal (Right, Left)
     
-    response |> should equal (Right, Left)
-
-[<Fact>]
-let ``Left at 1 takes 2 and 3 both going up`` () =
-    let response = determineElevatorResponse 1 0 (2, Up) (3, Up)
-    response |> should equal (Left, Left)
+    //The same elevator is closer to both calls
+    determineElevatorResponse 3 4 (3, Down) (2, Down) |> should equal (Left, Left)
+    determineElevatorResponse 1 4 (4, Down) (1, Down) |> should equal (Right, Left)
+    
+    //The elevator doesn't have to change it's direction to get them both.
+    determineElevatorResponse 2 2 (4, Down) (1, Down) |> should equal (Right, Left)
+    determineElevatorResponse 0 0 (2, Up) (3, Up) |> should equal (Right, Right)
+    determineElevatorResponse 0 0 (3, Up) (2, Up) |> should equal (Right, Right)
+    determineElevatorResponse 0 0 (3, Up) (0, Up) |> should equal (Right, Right)
+    
+    //The elevator doesn't have to go against a taken call direction to get them both.
+    determineElevatorResponse 2 2 (2, Down) (4, Down) |> should equal (Right, Left)
+    determineElevatorResponse 0 3 (2, Down) (4, Down) |> should equal (Right, Left)
--- Katas/Kata3/Elevator.fs
+++ Katas/Kata3/Elevator.fs
@@ -1,8 +1,6 @@
 ﻿module Katas.Kata3.Elevator
 
-open System.Runtime.ExceptionServices
-
-type Direction = Up | Down
+type Direction = Up | Down | Stay
 
 type Floor = int
 
@@ -12,8 +10,17 @@ type ElevatorCall = Floor * Direction
 
 type ElevatorState = Floor * Floor
 
-let distanceToCall position call =
-    abs (position - (fst call))
+type Route = { Distance:int;  Direction:Direction }
+
+type CallRoutes = { RouteToCallOne:Route; RouteToCallTwo:Route;}
+
+let determineRoute currentElevatorFloor call =
+    let floorCalledFrom = (fst call)
+    
+    {
+        Distance = abs(currentElevatorFloor - floorCalledFrom)
+        Direction = if floorCalledFrom > currentElevatorFloor then Up else if floorCalledFrom = currentElevatorFloor then Stay else Down
+    }
     
 let callDirectionsAreTheSame x y =
     (snd x) = (snd y)
@@ -23,14 +30,41 @@ let otherElevator (elevator:Elevator) =
     | Left -> Right
     | Right -> Left
 
+let calculateCallRoutesForElevator currentFloor callOne callTwo =
+    let callOneDistance = determineRoute currentFloor callOne
+    let callTwoDistance = determineRoute currentFloor callTwo
+    {
+        RouteToCallOne = callOneDistance
+        RouteToCallTwo = callTwoDistance
+    }
+
+let directionsAreCompatible (directions:Direction list) =
+    let hasUp = List.contains Up directions
+    let hasDown = List.contains Down directions
+    not (hasUp && hasDown)
+
+let doesNotChangeDirections (x:CallRoutes) =
+    directionsAreCompatible [x.RouteToCallOne.Direction; x.RouteToCallTwo.Direction]
+    
+let doesNotGoAgainstCalledDirection (call:ElevatorCall) (x:CallRoutes) = 
+    directionsAreCompatible [x.RouteToCallOne.Direction; x.RouteToCallTwo.Direction; (snd call)]
+
+
 let determineElevatorResponse (leftElevatorCurrentFloor:Floor) (rightElevatorCurrentFloor:Floor) (callOne:ElevatorCall) (callTwo:ElevatorCall) =
-    let leftDistanceToCallOne = distanceToCall leftElevatorCurrentFloor callOne
-    let leftDistanceToCallTwo = distanceToCall leftElevatorCurrentFloor callTwo
+    let leftRoutes = calculateCallRoutesForElevator leftElevatorCurrentFloor callOne callTwo
+    let rightRoutes = calculateCallRoutesForElevator rightElevatorCurrentFloor callOne callTwo
+    let routeLookup = Map.ofList [Left,leftRoutes; Right,rightRoutes] 
     
-    let rightDistanceToCallOne = distanceToCall rightElevatorCurrentFloor callOne
-    let rightDistanceToCallTwo = distanceToCall rightElevatorCurrentFloor callTwo
+    let firstCallElevator = if leftRoutes.RouteToCallOne.Distance < rightRoutes.RouteToCallOne.Distance then Left else Right
     
-    let firstCallElevator = if leftDistanceToCallOne < rightDistanceToCallOne then Left else Right
-    let secondCallElevator = if not (callDirectionsAreTheSame callOne callTwo) then (otherElevator firstCallElevator) else firstCallElevator
+    let other = otherElevator firstCallElevator
+    let firstCallElevatorRoutes = routeLookup.[firstCallElevator]
+    let otherElevatorRoutes = routeLookup.[other]
+    let firstCallElevatorQualifiesForSecondCall = (callDirectionsAreTheSame callOne callTwo)
+                                                  && firstCallElevatorRoutes.RouteToCallTwo.Distance <= otherElevatorRoutes.RouteToCallTwo.Distance
+                                                  && doesNotChangeDirections firstCallElevatorRoutes
+                                                  && doesNotGoAgainstCalledDirection callOne firstCallElevatorRoutes
+    
+    let secondCallElevator = if firstCallElevatorQualifiesForSecondCall then firstCallElevator else other
 
     firstCallElevator, secondCallElevator
